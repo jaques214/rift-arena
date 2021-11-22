bbf0557f630794e99d3faf4fd4c5f1c5741b080a
Tiago Coelho <tiagomcoelho22@gmail.com>
2021-11-22 20:49:13 +0000

    design navbar component (only static)

--- angular-client-side/src/app/app-routing.module.ts
+++ angular-client-side/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'nav-bar', component: NavBarComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
