d0007c2c321146894fa8068fb209afad1b950b99
TIAGO MACHADO COELHO <8190340@estg.ipp.pt>
2021-11-29 18:43:50 +0000

    started user services and auth services

---
+++ angular-client-side/src/app/services/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '@src/app/models/user';
+
+const endpoint = 'http://localhost:5000/api/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  // retorna o user com o username e password correspondente caso exista no servidor
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<User>(
+      endpoint + 'login',
+      { username: username, password: password },
+      httpOptions
+    );
+  }
+
+  // retorna um novo user com o email, username e password inseridos, em caso de erro nao retorna nada
+  register(
+    email: string,
+    username: string,
+    password: string
+  ): Observable<User> {
+    return this.http.post<User>(
+      endpoint + 'register',
+      { email: email, username: username, password: password },
+      httpOptions
+    );
+  }
+
+  // remove o user da localStorage, ou seja, remove a sua sessao
+  logout() {
+    localStorage.removeItem('currentUser');
+  }
+
+  // verifica se o email inserido pertence a um user com conta RIOT vinculada ou nao
+  verifyBoundedAccount(email: string): Observable<any> {
+    return this.http.post<any>(
+      endpoint + `verifyAccount`,
+      { email: email },
+      httpOptions
+    );
+  }
+}
---
+++ angular-client-side/src/app/services/user-rest/user-rest.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { User } from '@src/app/models/user';
+import { LinkedList } from 'linked-list-typescript';
+
+const endpoint = 'http://localhost:5000/api/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserRestService {
+  constructor(private http: HttpClient) {}
+
+  // retorna um user com o mesmo id inserido, caso contrário nada retorna
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(endpoint + 'id', httpOptions);
+  }
+
+  // retorna todos os users presentes no servidor
+  getUsers(): Observable<LinkedList<User>> {
+    return this.http.get<LinkedList<User>>(endpoint, httpOptions);
+  }
+
+  // envia um user e retorna o mesmo user com a informação atualizada no servidor
+  updateUser(user: User): Observable<User> {
+    return this.http.post<User>(
+      endpoint + `${user.userId}`,
+      JSON.stringify(user),
+      httpOptions
+    );
+  }
+  
+}
