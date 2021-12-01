f1b990a3ef0141a19fad1090ac74c3dc9f7d8526
amariatavares <58808823+amariatavares@users.noreply.github.com>
2021-12-01 18:21:37 +0000

    Alteração das pastas no backend

---
+++ backend-api/RiftARENA/Models/Services/teamService.cs
@@ -0,0 +1,122 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using RiftArena.Models;
+using RiftArena.Models.Contexts;
+
+namespace RiftArena.Models.Services
+{
+    public interface ITeamService
+    {
+        Team CreateTeam(Team team);
+        IEnumerable<Team> GetAll();
+        Team GetByID(long id);
+        Team UpdateTeam(int id,Team team);
+        void DeleteTeam(long id);
+        void AddMember(long id,User user);
+
+    }
+    public class TeamServices
+    {
+        private RiftArenaContext _context;
+
+        public TeamServices(RiftArenaContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Team> GetAll()
+        {
+            return  _context.Teams.ToList();
+        }
+
+        public Team GetByID(long id)
+        {
+            return _context.Teams.Find(id);
+        }
+
+        public Team CreateTeam(Team team)
+        {
+            if (string.IsNullOrWhiteSpace(team.Name))
+                throw new AppException("Team name is required");
+
+            if (_context.Teams.Any(x => x.Name == team.Name))
+                throw new AppException("Team name \"" + team.Name + "\" is already taken");
+
+            if (string.IsNullOrWhiteSpace(team.Tag))
+                throw new AppException("Team tag is required");
+
+            if (_context.Teams.Any(x => x.Tag == team.Tag))
+                throw new AppException("Team tag \"" + team.Tag + "\" is already taken");
+
+
+            _context.Teams.Add(team);
+            _context.SaveChanges();
+
+            return GetByID(team.TeamId);
+            
+        }
+
+        public Team UpdateTeam(int id,Team team)
+        {
+            var teamSer = _context.Teams.Find(id);
+            if (teamSer != null)
+                throw new AppException("Team not found!");
+
+
+            if (team.Name != teamSer.Name)
+            {
+
+                if (_context.Teams.Any(x => x.Name == team.Name))
+                    throw new AppException("Team name " + team.Name + " is already taken");
+            }
+
+            if (team.Tag != teamSer.Tag)
+            {
+
+                if (_context.Teams.Any(x => x.Tag == team.Tag))
+                    throw new AppException("Team tag " + team.Tag + " is already taken");
+            }
+
+            teamSer.Name = team.Name;
+            teamSer.Tag = team.Tag;
+
+            _context.Teams.Update(team);
+            _context.SaveChanges();
+
+            return GetByID(team.TeamId);
+        }
+
+        public void DeleteTeam(long id)
+        {
+            var team = _context.Teams.Find(id);
+            if (team != null)
+            {
+                _context.Teams.Remove(team);
+                _context.SaveChanges();
+            }
+        }
+
+        /*public void AddMember(long id,User user)
+        {
+            var TeamTemp = GetByID(id);
+            if (TeamTemp == null)
+            {
+                throw new AppException("Not Found");
+            }
+            else
+            {
+                if (TeamTemp.NumberMembers == 7)
+                {
+                    throw new AppException("Team full");
+                }
+                else
+                {
+                    TeamTemp.NumberMembers++;
+                }
+            }
+        }*/
+    }
+}
---
+++ backend-api/RiftARENA/Startup.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Server.IISIntegration;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using RiftArena.Models.Contexts;
+using RiftArena.Models.Services;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+
+namespace RiftArena
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddDbContext<RiftArenaContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("RiftArena")));
+            services.AddControllers();
+            /*services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RiftARENA", Version = "v1" });
+            });*/
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(Options =>
+            {
+                Options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Secret").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            services.AddTransient<IUserService, UserServices>();
+            services.AddAuthentication(IISDefaults.AuthenticationScheme);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                /*app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RiftARENA v1"));*/
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
