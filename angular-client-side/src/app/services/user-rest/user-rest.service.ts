dca73ab96c411238b71d47138aab104bfeeebfea
TIAGO MACHADO COELHO <8190340@estg.ipp.pt>
2021-12-01 11:02:38 +0000

    changed user route

--- angular-client-side/src/app/services/user-rest/user-rest.service.ts
+++ angular-client-side/src/app/services/user-rest/user-rest.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { User } from '@src/app/models/user';
+import { User } from '@models/user';
 import { LinkedList } from 'linked-list-typescript';
 
 const endpoint = 'http://localhost:5000/api/';
