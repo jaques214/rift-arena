7d7a386629d968596bc02f55903dac45f5b30988
amariatavares <58808823+amariatavares@users.noreply.github.com>
2021-12-25 18:47:23 +0000

    Alteração no TeamController e comentários no teamService para realização

--- backend-api/RiftARENA/Models/Services/teamService.cs
+++ backend-api/RiftARENA/Models/Services/teamService.cs
@@ -10,13 +10,13 @@ namespace RiftArena.Models.Services
 {
     public interface ITeamService
     {
-        Team CreateTeam(Team team);
+        Team CreateTeam(Team team, string userID);
         IEnumerable<Team> GetAll();
         Team GetByID(int id);
-        Team UpdateTeam(int id, Team team);
-        void DeleteTeam(int id);
-        void AddMember(int id, User user);
-        void RemoveMember(int id, User user);
+        Team UpdateTeam(int id, Team team, string userID);
+        void DeleteTeam(int id, string userID);
+        void AddMember(int id, User user, string userID);
+        void RemoveMember(int id, User user, string userID);
 
     }
     public class TeamServices : ITeamService
@@ -27,6 +27,7 @@ namespace RiftArena.Models.Services
         {
             _context = context;
         }
+
         /// <summary>
         /// Método que retorna todas as equipas existentes
         /// </summary>
@@ -35,6 +36,7 @@ namespace RiftArena.Models.Services
         {
             return _context.Teams.ToList();
         }
+        
         /// <summary>
         /// Método que retorna uma equipa através de um ID
         /// </summary>
@@ -44,13 +46,14 @@ namespace RiftArena.Models.Services
         {
             return _context.Teams.Find(id);
         }
+
         /// <summary>
         /// Método que permite a criação de uma equipa
         /// </summary>
         /// <param name="team">Equipa a ser criada</param>
         /// <returns>Equipa criada</returns>
         /// <exception cref="AppException">Exceção caso a equipa a criar falhe nas validações</exception>
-        public Team CreateTeam(Team team)
+        public Team CreateTeam(Team team, string userID)
         //falta usar o token para verificar se o user logado ja esta numa equipa e se ja tem conta vinculada
         {
             team.Members = new List<User>();
@@ -74,7 +77,7 @@ namespace RiftArena.Models.Services
                 throw new AppException("TeamLeader\"" + team.TeamLeader + "\"is already taken");
 
 
-            var leader = _context.Users.FirstOrDefault(x => x.Nickname == team.TeamLeader);
+            var leader = _context.Users.Find(userID);
 
             team.Members.Add(leader);
             team.Defeats = 0;
@@ -82,8 +85,7 @@ namespace RiftArena.Models.Services
             team.TournamentsWon = 0;
             team.GamesPlayed = 0;
             team.NumberMembers = 1;
-            //team.Rank = token user getrank(atraves da api)
-
+            //team.Rank = token user getrank(atraves da api) ==> team.Rank = leader.Rank; (?)s
 
             _context.Teams.Add(team);
             _context.SaveChanges();
@@ -91,6 +93,7 @@ namespace RiftArena.Models.Services
             return GetByID(team.TeamId);
 
         }
+
         /// <summary>
         /// Método que permite a edição de uma equipa
         /// </summary>
@@ -98,8 +101,9 @@ namespace RiftArena.Models.Services
         /// <param name="team">Equipa com as edições feitas</param>
         /// <returns>Equipa editada</returns>
         /// <exception cref="AppException">Exceção caso a equipa a editar falhe nas validações</exception>
-        public Team UpdateTeam(int id, Team team)
+        public Team UpdateTeam(int id, Team team, string userID)
         {
+            //SÓ PERMITIR SE team.TeamLeader.UserID = userID
             var teamSer = GetByID(id);
             if (teamSer == null)
                 throw new AppException("Team not found!");
@@ -129,12 +133,15 @@ namespace RiftArena.Models.Services
 
             return GetByID(teamSer.TeamId);
         }
+
         /// <summary>
         /// Método que permite a eliminação de uma equipa
         /// </summary>
         /// <param name="id">ID da equipa a eliminar</param>
-        public void DeleteTeam(int id)
+        public void DeleteTeam(int id, string userID)
         {
+
+            //VERIFICAR SE userID = team.TeamLeader.UserID
             var team = _context.Teams.Find(id);
             if (team != null)
             {
@@ -143,14 +150,16 @@ namespace RiftArena.Models.Services
                 _context.SaveChanges();
             }
         }
+
         /// <summary>
         /// Método que permite a adição de um membro a uma equipa
         /// </summary>
         /// <param name="id">ID da equipa que o user será adicionado</param>
         /// <param name="user">User que será adicionado</param>
         /// <exception cref="AppException">Exceção caso a equipa não exista ou esteja cheia</exception>
-        public void AddMember(int id, User user)
+        public void AddMember(int id, User user, string userID)
         {
+            //SÓ PERMITIR SE team.TeamLeader.UserID = userID (?)
             var TeamTemp = GetByID(id);
             if (TeamTemp == null)
             {
@@ -171,15 +180,16 @@ namespace RiftArena.Models.Services
             _context.Teams.Update(TeamTemp);
             _context.SaveChanges();
         }
+        
         /// <summary>
         /// Método que permite a remoção de um membro a uma equipa
         /// </summary>
         /// <param name="id">ID da equipa que o user será removido</param>
         /// <param name="user">User que será removido</param>
         /// <exception cref="AppException">Exceção caso a equipa não exista ou o user a ser removido seja o team leader</exception>
-        public void RemoveMember(int id, User user)
+        public void RemoveMember(int id, User user, string userID)
         {
-            //falta usar o token para verificar se o user logado é team leader
+            //falta usar o token para verificar se o user logado é team leader ==> team.TeamLeader.UserID = userID
             var TeamTemp = GetByID(id);
             if (TeamTemp == null)
             {
