cd467186e438654d02cf7fb0c002f011a37b7465
8190214 <8190214@estg.ipp.pt>
2021-12-27 17:35:59 +0000

    fix icon error

--- angular-client-side/src/app/components/shared-form-field/shared-form-field.component.ts
+++ angular-client-side/src/app/components/shared-form-field/shared-form-field.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-//import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-shared-form-field',
@@ -11,20 +10,10 @@ export class SharedFormFieldComponent implements OnInit {
   @Input() type?:any;
   hide = true;
   regionValues = ['br1', 'eun1', 'euw1', 'jp1', 'kr', 'la1', 'la2', 'na1', 'oc1', 'ru', 'tr1'];
-  //email = new FormControl('', [Validators.required, Validators.email]);
 
   constructor() { }
 
   ngOnInit(): void {
     this.type || (this.type = this.input.type)
   }
-
-  /*getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }*/
-
 }
--- angular-client-side/src/app/components/view-profile/view-profile.component.ts
+++ angular-client-side/src/app/components/view-profile/view-profile.component.ts
@@ -1,11 +1,203 @@
-import { TeamRestService } from './../../services/team-rest/team-rest.service';
+// import { TeamRestService } from './../../services/team-rest/team-rest.service';
+// import { Observable } from 'rxjs';
+// import { Component, OnInit, Input } from '@angular/core';
+// import { UserRestService } from '@services/user-rest/user-rest.service';
+// import {User} from '@models/user'
+// import { LinkedAccount } from '@models/linked_acount';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { Team } from '@models/team';
+
+// @Component({
+//   selector: 'app-view-profile',
+//   templateUrl: './view-profile.component.html',
+//   styleUrls: ['./view-profile.component.css']
+// })
+// export class ViewProfileComponent implements OnInit {
+//   @Input() input!: any;
+//   user?: User;
+//   team?: Team;
+//   account!: LinkedAccount;
+//   info: any;
+//   icon?: string;
+//   label?: string;
+//   fileSelected?: File;
+//   image = (this.user?.profileImage as unknown as string);
+//   imageFieldName: string = this.normalizeImageName(this.image);
+//   imageFieldPath = 'https://localhost:5001/api/' + this.image;
+//   flag : string = "view";
+//   accountFlag: string = "view";
+//   formFields: any = User.fields();
+//   accountFields: any = LinkedAccount.fields();
+
+//   constructor(private restService : UserRestService, private teamRestService : TeamRestService, private route: ActivatedRoute, private router: Router) {
+//     const routeState = this.router?.getCurrentNavigation()?.extras?.state
+//     if (routeState) {
+//       this.user = routeState['user'];
+//       this.populateForm()
+//     }
+//    }
+
+//    populateForm(){
+//     //if a user already exists populates the formFields inputs.
+//     this.formFields.inputs.forEach((input:any) => {
+//       input.model! = (this.user as any)[input.name!];
+//     });
+//   }
+
+//    onSubmit(): void {
+//     const data = this.user!;
+//     this.formFields.inputs.forEach((input:any) => {
+//       (data as any)[input.name!] = input.model;
+//     });
+//     this.flag = "view";
+//     this.editUser(data);
+//   }
+
+//   clickEvent() {
+//       this.flag = (this.flag == "view") ? "edit" : "view";
+//   }
+
+//   clickAccount() {
+//     this.accountFlag = (this.accountFlag == "view") ? "edit" : "view";
+// }
+
+//   editUser(user: User): void {
+//     this.restService.updateUser(user).subscribe({
+//       next: () => {
+//         this.getUser(user.userID!).subscribe((user) => {
+//           this.user = user;
+//           this.populateForm();
+//         });
+//       },
+//       error: error => {
+//         // TODO: have error handling 
+//       }
+//     });
+//   }
+
+//   getUser(userId: number): Observable<any> {
+//     return this.restService.getUser(userId);
+//   }
+
+//   ngOnInit(): void {
+//     const id = this.route.snapshot.params['id'];
+//     if(id && !this.user){
+//       this.getUser(id).subscribe((user) => {
+//         this.user = user;
+//         //this.user?.team = this.getTeam(1)
+//         this.populateForm();
+//         if(this.account == undefined) {
+//           this.info = "No Linked Account";
+//           this.icon = "add_circle_outline";
+//           this.label = "remove_circle_outline";
+//         }
+//         else {
+//           this.info = [this.account.username, this.account.rank, this.account.region]
+//           this.icon = "remove_circle_outline";
+//           this.label = "remove circle outline icon";
+//         }
+//         console.log(this.user);
+//         console.log(this.user?.teamID);
+//         console.log(this.user?.userID);
+//         console.log(this.user?.team?.name);
+//         this.getTeam(1).subscribe((team) => {
+//           this.team = team;
+//         });
+//       });
+//     }
+//   }
+
+//   getUserValue(value: any) {
+//     let convert: string = "";
+    
+//     if(this.user != null) {
+//         let values = Object.entries(this.user!);
+//         //console.log(values);
+
+//         values.forEach(val => {
+//           if(val[0] == value) {
+//             convert = val[1];
+//         }
+//         });
+//     }
+//     //console.log(convert);
+//     return convert;
+//   }
+
+//   normalizeImageName(imagePath: string): string {
+//     return imagePath?.split('_')?.splice(2)?.join('_');
+//   }
+
+//   onFileSelected(user: any): void {
+//     const target: HTMLInputElement | null = user.target as HTMLInputElement;
+//     this.fileSelected = target?.files?.[0] as File;
+//   }
+
+//   getTeam(teamId: number): Observable<any> {
+//     return this.teamRestService.getTeam(teamId);
+//   }
+
+//   getTeamName() {
+//     return (this.user?.team) ? this.user?.team.name : "No Team";
+//   }
+
+//   /*addAccount(): void {
+//     if(this.accountFlag = "view") {
+//       this.icon = "add_circle_outline";
+//       this.label = "add circle outline icon";
+//       this.accountFlag = "edit";
+//     }
+//     else {
+//       this.icon = "remove_circle_outline";
+//       this.label = "remove circle outline icon";
+//       this.accountFlag = "view";
+//     }*/
+//     //this.restService.addAccount(account).subscribe(() => {
+//       // if(account == undefined) {
+//       //   this.info = "No Linked Account";
+//       // }
+//       // this.info = {
+//       //   username: account.username,
+//       //   rank: account.rank,
+//       //   region: account.region
+//       // }
+//       //window.location.reload();
+//   //});
+//   //}
+
+//   addAccount(account: LinkedAccount): void {
+//     //this.accountFlag =  (this.accountFlag = "view") ? "edit" : "view";
+//     if(this.accountFlag = "view") {
+//       this.icon = "add_circle_outline";
+//       this.label = "add circle outline icon";
+//       this.accountFlag = "edit";
+//     }
+//     else {
+//       this.icon = "remove_circle_outline";
+//       this.label = "remove circle outline icon";
+//       this.accountFlag = "view";
+//     }
+//     //this.restService.addAccount(account).subscribe(() => {
+//       // if(account == undefined) {
+//       //   this.info = "No Linked Account";
+//       // }
+//       // this.info = {
+//       //   username: account.username,
+//       //   rank: account.rank,
+//       //   region: account.region
+//       // }
+//       //window.location.reload();
+//   //});
+//   }
+
+// }
+
 import { Observable } from 'rxjs';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserRestService } from '@services/user-rest/user-rest.service';
 import {User} from '@models/user'
 import { LinkedAccount } from '@models/linked_acount';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Team } from '@models/team';
 
 @Component({
   selector: 'app-view-profile',
@@ -13,67 +205,17 @@ import { Team } from '@models/team';
   styleUrls: ['./view-profile.component.css']
 })
 export class ViewProfileComponent implements OnInit {
-  @Input() input!: any;
-  user?: User;
-  team?: Team;
-  account!: LinkedAccount;
-  info: any;
-  icon?: string;
-  label?: string;
+  user1 = {
+    nickname: "nickname1"
+  }
+  user!: User;
+  list: LinkedAccount[] = [];
   fileSelected?: File;
   image = (this.user?.profileImage as unknown as string);
+  imageFieldPath = 'http://localhost:5001/api/' + this.image;
   imageFieldName: string = this.normalizeImageName(this.image);
-  imageFieldPath = 'https://localhost:5001/api/' + this.image;
-  flag : string = "view";
-  accountFlag: string = "view";
-  formFields: any = User.fields();
-  accountFields: any = LinkedAccount.fields();
 
-  constructor(private restService : UserRestService, private teamRestService : TeamRestService, private route: ActivatedRoute, private router: Router) {
-    const routeState = this.router?.getCurrentNavigation()?.extras?.state
-    if (routeState) {
-      this.user = routeState['user'];
-      this.populateForm()
-    }
-   }
-
-   populateForm(){
-    //if a user already exists populates the formFields inputs.
-    this.formFields.inputs.forEach((input:any) => {
-      input.model! = (this.user as any)[input.name!];
-    });
-  }
-
-   onSubmit(): void {
-    const data = this.user!;
-    this.formFields.inputs.forEach((input:any) => {
-      (data as any)[input.name!] = input.model;
-    });
-    this.flag = "view";
-    this.editUser(data);
-  }
-
-  clickEvent() {
-      this.flag = (this.flag == "view") ? "edit" : "view";
-  }
-
-  clickAccount() {
-    this.accountFlag = (this.accountFlag == "view") ? "edit" : "view";
-}
-
-  editUser(user: User): void {
-    this.restService.updateUser(user).subscribe({
-      next: () => {
-        this.getUser(user.userID!).subscribe((user) => {
-          this.user = user;
-          this.populateForm();
-        });
-      },
-      error: error => {
-        // TODO: have error handling 
-      }
-    });
-  }
+  constructor(private restService : UserRestService, private route: ActivatedRoute, private router: Router) { }
 
   getUser(userId: number): Observable<any> {
     return this.restService.getUser(userId);
@@ -84,44 +226,8 @@ export class ViewProfileComponent implements OnInit {
     if(id && !this.user){
       this.getUser(id).subscribe((user) => {
         this.user = user;
-        //this.user?.team = this.getTeam(1)
-        this.populateForm();
-        if(this.account == undefined) {
-          this.info = "No Linked Account";
-          this.icon = "add_circle_outline";
-          this.label = "remove_circle_outline";
-        }
-        else {
-          this.info = [this.account.username, this.account.rank, this.account.region]
-          this.icon = "remove_circle_outline";
-          this.label = "remove circle outline icon";
-        }
-        console.log(this.user);
-        console.log(this.user?.teamID);
-        console.log(this.user?.userID);
-        console.log(this.user?.team?.name);
-        this.getTeam(1).subscribe((team) => {
-          this.team = team;
-        });
       });
     }
-  }
-
-  getUserValue(value: any) {
-    let convert: string = "";
-    
-    if(this.user != null) {
-        let values = Object.entries(this.user!);
-        //console.log(values);
-
-        values.forEach(val => {
-          if(val[0] == value) {
-            convert = val[1];
-        }
-        });
-    }
-    //console.log(convert);
-    return convert;
   }
 
   normalizeImageName(imagePath: string): string {
@@ -133,61 +239,18 @@ export class ViewProfileComponent implements OnInit {
     this.fileSelected = target?.files?.[0] as File;
   }
 
-  getTeam(teamId: number): Observable<any> {
-    return this.teamRestService.getTeam(teamId);
-  }
-
-  getTeamName() {
-    return (this.user?.team) ? this.user?.team.name : "No Team";
-  }
-
-  /*addAccount(): void {
-    if(this.accountFlag = "view") {
-      this.icon = "add_circle_outline";
-      this.label = "add circle outline icon";
-      this.accountFlag = "edit";
-    }
-    else {
-      this.icon = "remove_circle_outline";
-      this.label = "remove circle outline icon";
-      this.accountFlag = "view";
-    }*/
-    //this.restService.addAccount(account).subscribe(() => {
-      // if(account == undefined) {
-      //   this.info = "No Linked Account";
-      // }
-      // this.info = {
-      //   username: account.username,
-      //   rank: account.rank,
-      //   region: account.region
-      // }
-      //window.location.reload();
-  //});
-  //}
-
-  addAccount(account: LinkedAccount): void {
-    //this.accountFlag =  (this.accountFlag = "view") ? "edit" : "view";
-    if(this.accountFlag = "view") {
-      this.icon = "add_circle_outline";
-      this.label = "add circle outline icon";
-      this.accountFlag = "edit";
-    }
-    else {
-      this.icon = "remove_circle_outline";
-      this.label = "remove circle outline icon";
-      this.accountFlag = "view";
-    }
-    //this.restService.addAccount(account).subscribe(() => {
-      // if(account == undefined) {
-      //   this.info = "No Linked Account";
-      // }
-      // this.info = {
-      //   username: account.username,
-      //   rank: account.rank,
-      //   region: account.region
-      // }
-      //window.location.reload();
-  //});
+  editUser(user: User): void {
+    this.restService.updateUser(user).subscribe({
+      next: () => {
+        this.getUser(user.userID!).subscribe((user) => {
+          this.user = user;
+        });
+      },
+      error: error => {
+        // TODO: have error handling 
+      }
+    });
   }
 
 }
+
