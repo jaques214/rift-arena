1092bde2825816649344295444123aabe5a77687
Tiago Teixeira <8190592@estg.ipp.pt>
2022-01-09 19:50:12 +0000

    Poster do User na B

---
+++ backend-api/RiftARENA/Migrations/20220107192344_UpdateSecondCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RiftARENA.Migrations
+{
+    public partial class UpdateSecondCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MiniumTier",
+                table: "Tournaments");
+
+            migrationBuilder.RenameColumn(
+                name: "Tier",
+                table: "Users",
+                newName: "Poster");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Poster",
+                table: "Users",
+                newName: "Tier");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rank",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MiniumTier",
+                table: "Tournaments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
