09a2382fb0e1a6e3e2314ea9597177a69d816b26
amariatavares <58808823+amariatavares@users.noreply.github.com>
2022-01-16 17:48:02 +0000

    Update Database add column maxteams in tournaments table

---
+++ backend-api/RiftARENA/Migrations/20220116174426_UpdateCreatev4.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RiftARENA.Migrations
+{
+    public partial class UpdateCreatev4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                            name: "MaxTeams",
+                            table: "Tournaments",
+                            type: "int",
+                            nullable: false,
+                            defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           migrationBuilder.DropColumn(
+                name: "MaxTeams",
+                table: "Tournaments");
+        }
+    }
+}
