bfb937c2f4f6faa7bc68aa5aa7ccaea6c63cfb00
jaques214 <jaquesafr@gmail.com>
2026-03-01 10:52:12 +0000

    refactor(nav-bar): clean up code formatting; remove console logs and update dependencies

--- angular-client-side/src/app/components/nav-bar/nav-bar.component.ts
+++ angular-client-side/src/app/components/nav-bar/nav-bar.component.ts
@@ -3,12 +3,12 @@ import { User } from '@models/user';
 import { AuthService } from '@services/auth/auth.service';
 import { UserRestService } from '@services/user-rest/user-rest.service';
 import { environment } from '@src/environments/environment';
-import {NgIf, NgOptimizedImage} from '@angular/common';
-import {MatIconModule} from "@angular/material/icon";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
-import {MatMenuModule} from "@angular/material/menu";
-import {RouterLink} from "@angular/router";
+import { NgIf, NgOptimizedImage } from '@angular/common';
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { RouterLink } from "@angular/router";
 import {
   trigger,
   state,
@@ -16,29 +16,29 @@ import {
   animate,
   transition
 } from '@angular/animations';
-import {Request} from "@models/request";
+import { Request } from "@models/request";
 
 @Component({
-    selector: 'app-nav-bar',
-    templateUrl: './nav-bar.component.html',
-    styleUrl: './nav-bar.component.css',
-    imports: [NgIf, NgOptimizedImage, MatIconModule, MatToolbarModule, MatButtonModule, MatMenuModule, RouterLink],
-    animations: [
-        trigger('openClose', [
-            state('open', style({
-                height: '100%',
-            })),
-            state('closed', style({
-                height: '0px',
-            })),
-            transition('closed => open', [
-                animate('0.3s')
-            ]),
-            transition('open => closed', [
-                animate('0.3s')
-            ]),
-        ]),
-    ]
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css',
+  imports: [NgIf, NgOptimizedImage, MatIconModule, MatToolbarModule, MatButtonModule, MatMenuModule, RouterLink],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '100%',
+      })),
+      state('closed', style({
+        height: '0px',
+      })),
+      transition('closed => open', [
+        animate('0.3s')
+      ]),
+      transition('open => closed', [
+        animate('0.3s')
+      ]),
+    ]),
+  ]
 })
 export class NavBarComponent implements OnInit {
   response!: { dbPath: '' };
@@ -71,7 +71,6 @@ export class NavBarComponent implements OnInit {
         error: () => localStorage.removeItem('currentUser')
       });
     }
-    console.log(this.user);
   }
 
   createImgPath = (serverPath: string) => {
@@ -85,7 +84,6 @@ export class NavBarComponent implements OnInit {
 
   openMenu() {
     this.isShow = !this.isShow;
-    console.log(this.isShow)
   }
 
   logout(): void {
